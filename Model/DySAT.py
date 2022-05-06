4dd9094ff60e92bdc96ea03d6d75a6f9a9e7e540
Fahao Chen <chenfh612@gmail.com>
2022-05-06 16:50:00 +0900

    update

--- Model/DySAT.py
+++ Model/DySAT.py
@@ -170,7 +170,7 @@ class DySAT(nn.Module):
             structural_outputs_padded.append(padded)
         structural_outputs_padded = torch.cat(structural_outputs_padded, dim=1) # [N, T, F]
 
-        print('rank: {} with tensor size {}'.format(self.args['rank'], structural_outputs_padded.size()))
+        # print('rank: {} with tensor size {}'.format(self.args['rank'], structural_outputs_padded.size()))
 
 
         # Temporal Attention forward
