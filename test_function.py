7b66b0aaa2783881e842c94e3bdeb49814efd22a
Fahao Chen <chenfh612@gmail.com>
2022-05-07 21:40:07 +0900

    update

--- test_function.py
+++ test_function.py
@@ -239,7 +239,7 @@ def run_dgnn(args):
 
     # TODO: Unevenly slice graphs
     # load graphs
-    load_g, load_adj, load_feats = load_graphs(args)
+    load_g, load_adj, load_feats = slice_graph(*load_graphs(args))
     # print('features: ',load_feats)
     num_graph = len(load_g)
     print("Loaded {}/{} graphs".format(num_graph, args['time_steps']))
