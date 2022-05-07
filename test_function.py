5529fbb44977e52ea0d36938d1c8d02cb7310160
Fahao Chen <chenfh612@gmail.com>
2022-05-07 20:36:33 +0900

    update

--- test_function.py
+++ test_function.py
@@ -238,7 +238,7 @@ def run_dgnn(args):
     # TODO: Unevenly slice graphs
     # load graphs
     load_g, load_adj, load_feats = slice_graph(*load_graphs(args))
-    print('features: ',load_feats)
+    # print('features: ',load_feats)
     num_graph = len(load_g)
     print("Loaded {}/{} graphs".format(num_graph, args['time_steps']))
     args['structural_time_steps'] = num_graph
@@ -287,8 +287,8 @@ def run_dgnn(args):
             graphs = [graph.to(device) for graph in graphs]
             train_start_time = time.time()
             out = model(graphs, batch_x)
-            print('outputs: ',out)
-            print('true_labels: ',batch_y)
+            # print('outputs: ',out)
+            # print('true_labels: ',batch_y)
             loss = loss_func(out.squeeze(dim=-1), batch_y)
             Loss.append(loss.item())
             optimizer.zero_grad()
