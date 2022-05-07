4f822897cf072a631665bcf7dadb65c7d448e378
Fahao Chen <chenfh612@gmail.com>
2022-05-07 22:00:19 +0900

    update

--- test_function.py
+++ test_function.py
@@ -70,7 +70,7 @@ def _gate(args):
 # TODO: complete the global forward
 def run_dgnn_distributed(args):
     args['method'] = 'dist'
-    args['connection'] = False
+    args['connection'] = True
     args['gate'] = False
     device = args['device']
     rank = args['rank']
