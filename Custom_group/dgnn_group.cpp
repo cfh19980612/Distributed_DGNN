88a45c5ca09dbb0162f2cf04675ac75d3857afab
Fahao Chen <chenfh612@gmail.com>
2022-05-09 14:05:21 +0900

    update

--- Custom_group/dgnn_group.cpp
+++ Custom_group/dgnn_group.cpp
@@ -97,12 +97,76 @@ c10::intrusive_ptr<ProcessGroup::Work> ProcessGroupDGNN::broadcast(
   throw std::runtime_error("not supported");
 }
 
-c10::intrusive_ptr<ProcessGroup::Work> ProcessGroupDGNN::gather(
-    std::vector<std::vector<at::Tensor>>& /* unused */,
-    std::vector<at::Tensor>& /* unused */,
-    const GatherOptions& /* unused */) {
-  throw std::runtime_error("not supported");
+// c10::intrusive_ptr<ProcessGroup::Work> ProcessGroupDGNN::gather(
+//     std::vector<std::vector<at::Tensor>>& /* unused */,
+//     std::vector<at::Tensor>& /* unused */,
+//     const GatherOptions& /* unused */) {
+//   throw std::runtime_error("not supported");
+// }
+
+c10::intrusive_ptr<ProcessGroup::Work> ProcessGroupGloo::gather(
+    std::vector<std::vector<at::Tensor>>& outputs,
+    std::vector<at::Tensor>& inputs,
+    const GatherOptions& opts) {
+  static auto invalidArgument = [](const std::string& msg) {
+    TORCH_CHECK(false, "ProcessGroupGloo::gather: " + msg);
+  };
+
+  assertRootRank(invalidArgument, opts.rootRank, size_);
+  assertSingleElementInput(invalidArgument, inputs);
+  assertDense(invalidArgument, inputs);
+
+  if (getRank() == opts.rootRank) {
+    if (outputs.size() != 1) {
+      std::stringstream ss;
+      ss << "requires a single-element output list containing a list with "
+         << getSize() << " tensors.";
+      invalidArgument(ss.str());
+    } else if (outputs[0].size() != static_cast<size_t>(getSize())) {
+      std::stringstream ss;
+      ss << "Incorrect output list size " << outputs[0].size()
+         << ". Output list size should be " << getSize()
+         << ", same as size of the process group.";
+      invalidArgument(ss.str());
+    }
+
+    const auto& options = inputs[0].options();
+    const auto& sizes = inputs[0].sizes();
+    assertTypeAndSizesMatch(invalidArgument, outputs[0], options, sizes);
+  } else {
+    if (outputs.size() != 0) {
+      invalidArgument("requires empty output on non-root");
+    }
+  }
+
+  const auto& device = inputs[0].device();
+  switch (device.type()) {
+    case at::kCPU:
+      break;
+    case at::kCUDA:
+      // If the user gave us a CUDA tensor then CUDA must be loaded.
+      TORCH_INTERNAL_ASSERT(at::hasCUDA());
+      break;
+    default:
+      invalidArgument(c10::str("unsupported device type ", device.type()));
+  }
+
+  c10::intrusive_ptr<AsyncGatherWork> work;
+  auto tag = nextTag();
+  auto context = getContext(tag);
+  if (device.type() == at::kCPU) {
+    work = c10::make_intrusive<AsyncGatherWork>(
+        std::move(context), outputs, inputs, opts.rootRank, tag);
+  } else if (device.type() == at::kCUDA) {
+    work = c10::make_intrusive<AsyncGatherCUDAWork>(
+        std::move(context), outputs, inputs, opts.rootRank, tag);
+  } else {
+    TORCH_CHECK(false, "Invalid backend");
+  }
+  enqueue(work);
+  return work;
 }
+
 
 c10::intrusive_ptr<ProcessGroup::Work> ProcessGroupDGNN::reduce(
     std::vector<at::Tensor>& /* unused */,
@@ -154,7 +218,7 @@ c10::intrusive_ptr<ProcessGroup> ProcessGroupDGNN::createProcessGroupDGNN(
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  // bind python and C++11, 'createProcessGroupDGNN' is the naem of python API
+  // bind python and C++11, 'createProcessGroupDGNN' is the name of python API
   m.def("createProcessGroupDGNN", &ProcessGroupDGNN::createProcessGroupDGNN);
 }
 
