9b327384b5684cfb0f22f6c2116cc63d1a77c541
Fahao Chen <chenfh612@gmail.com>
2022-06-07 15:41:20 +0900

    update

--- Model/DySAT.py
+++ Model/DySAT.py
@@ -16,7 +16,7 @@ from Model.layers import TemporalAttentionLayer
 from utils import *
 
 def _multi_process_gather(rank, dest, x_comm, mp_group, world_size, Num_nodes_per_worker, gather_dict):
-    group=mp_group[dest]
+    # group=mp_group[dest]
     print('Hello!')
     # if dest != world_size - 1:
     #     x_send = x_comm[dest*Num_nodes_per_worker:(dest+1)*Num_nodes_per_worker,:,:]
@@ -55,7 +55,7 @@ def _node_partition_comm_before(args, x):
     # torch.multiprocessing.set_start_method('spawn')
     workers = []
     for i in range(world_size):
-        p = mp.Process(target=_multi_process_gather, args=(rank, i, rank, mp_group, world_size, Num_nodes_per_worker, rank))
+        p = mp.Process(target=_multi_process_gather, args=(rank, i, rank, rank, world_size, Num_nodes_per_worker, rank))
         # p = mp.Process(target=_multi_process_gather, args=(rank, i, rank, rank, world_sirankze, rank, rank))
         p.start()
         workers.append(p)
