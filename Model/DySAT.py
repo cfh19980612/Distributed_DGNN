75cae7d0c48df40f9f639adf85b5f45c0bb1d0c0
Fahao Chen <chenfh612@gmail.com>
2022-06-07 15:14:05 +0900

    update

--- Model/DySAT.py
+++ Model/DySAT.py
@@ -17,19 +17,19 @@ from utils import *
 
 def _multi_process_gather(rank, dest, x_comm, mp_group, world_size, Num_nodes_per_worker, gather_dict):
     group=mp_group[dest]
-
-    if dest != world_size - 1:
-        x_send = x_comm[dest*Num_nodes_per_worker:(dest+1)*Num_nodes_per_worker,:,:]
-    else:
-        x_send = x_comm[dest*Num_nodes_per_worker:,:,:]
-    if rank == dest:
-        # gather_dict = [torch.zeros_like(x_send).to(device) for j in range(world_size)]
-        # comm_start = time.time()
-        torch.distributed.gather(x_send, gather_list=gather_dict, dst=dest, group=group)
-        # comm_time.append(time.time() - comm_start)
-        # args['comm_cost'] += time.time() - comm_start
-    else:
-        torch.distributed.gather(x_send, gather_list=None, dst=dest, group=group)
+    print('Hello!')
+    # if dest != world_size - 1:
+    #     x_send = x_comm[dest*Num_nodes_per_worker:(dest+1)*Num_nodes_per_worker,:,:]
+    # else:
+    #     x_send = x_comm[dest*Num_nodes_per_worker:,:,:]
+    # if rank == dest:
+    #     # gather_dict = [torch.zeros_like(x_send).to(device) for j in range(world_size)]
+    #     # comm_start = time.time()
+    #     torch.distributed.gather(x_send, gather_list=gather_dict, dst=dest, group=group)
+    #     # comm_time.append(time.time() - comm_start)
+    #     # args['comm_cost'] += time.time() - comm_start
+    # else:
+    #     torch.distributed.gather(x_send, gather_list=None, dst=dest, group=group)
 
     
 
@@ -55,7 +55,8 @@ def _node_partition_comm_before(args, x):
     # torch.multiprocessing.set_start_method('spawn')
     workers = []
     for i in range(world_size):
-        p = mp.Process(target=_multi_process_gather, args=(rank, i, x_comm.clone().detach(), mp_group, world_size, Num_nodes_per_worker, gather_lists))
+        # p = mp.Process(target=_multi_process_gather, args=(rank, i, x_comm.clone().detach(), mp_group, world_size, Num_nodes_per_worker, gather_lists))
+        p = mp.Process(target=_multi_process_gather, args=(rank, i, rank, mp_group, world_size, Num_nodes_per_worker, gather_lists))
         p.start()
         workers.append(p)
     for p in workers:
