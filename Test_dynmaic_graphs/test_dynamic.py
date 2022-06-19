31d3b72faf4ee4f22298d506896d25cb133a9be7
Fahao Chen <chenfh612@gmail.com>
2022-06-19 21:29:26 +0900

    update

--- Test_dynmaic_graphs/test_dynamic.py
+++ Test_dynmaic_graphs/test_dynamic.py
@@ -33,8 +33,8 @@ def stat_age_difference(graphs):
         for j in range(len(graphs))[i:]:
             # adj_temp = adj_sp_tensor[j].to_dense()[current_last_node:current_last_node + num_of_nodes]
             # edges = torch.count_nonzero(adj_temp, dim=1).reshape(-1, 1).squeeze()
-            print(graphs[j].degrees())
-            edges = torch.tensor(graphs[j].degrees())[current_last_node:current_last_node + num_of_nodes]
+            print(graphs[j].degree())
+            edges = torch.tensor(graphs[j].degree())[current_last_node:current_last_node + num_of_nodes]
             mask = torch.gt(edges, max_num_of_edges)
             print(max_num_of_edges, edges)
             max_num_of_edges[mask] = edges[mask]
