ab6c0d22d58387b3b7aa101df2a9f067b7b03ae9
Fahao Chen <chenfh612@gmail.com>
2022-06-20 11:46:21 +0900

    update

--- Test_dynmaic_graphs/Stakexch/preprocess.py
+++ Test_dynmaic_graphs/Stakexch/preprocess.py
@@ -12,7 +12,7 @@ node_cnt = 0
 node_idx = {}
 idx_node = []
 
-file_path = current_path + '/soc-wiki-elec.edges'
+file_path = current_path + '/ia-stackexch-user-marks-post-und.edges'
 save_graph_path = current_path + '/data/graphs.npz'
 save_features_path = current_path + '/data/features.npz'
 
