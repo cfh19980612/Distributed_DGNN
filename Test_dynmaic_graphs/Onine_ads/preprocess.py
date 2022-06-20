518598d861da26ee0134546dd77cbc0d6ac42ee9
Fahao Chen <chenfh612@gmail.com>
2022-06-20 12:53:54 +0900

    update

--- Test_dynmaic_graphs/Onine_ads/preprocess.py
+++ Test_dynmaic_graphs/Onine_ads/preprocess.py
@@ -12,7 +12,7 @@ node_cnt = 0
 node_idx = {}
 idx_node = []
 
-file_path = current_path + '/ia-stackexch-user-marks-post-und.edges'
+file_path = current_path + '/ia-online-ads-criteo-attrs.edges'
 save_graph_path = current_path + '/data/graphs.npz'
 save_features_path = current_path + '/data/features.npz'
 
@@ -22,7 +22,7 @@ with open(file_path) as f:
         if l[0] == '%':
             continue
 
-        x, y, e, t = map(int, l.split(' '))
+        x, y, t, _, _ = map(int, l.split(' '))
         # print (x,y,e,t)
         timestamp = datetime.fromtimestamp(t)
         ts.append(timestamp)
