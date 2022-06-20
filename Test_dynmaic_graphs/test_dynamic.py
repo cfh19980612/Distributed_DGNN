ffc90d5edc8c5c2d444a58f35d1a74f3d7aee9b4
Fahao Chen <chenfh612@gmail.com>
2022-06-20 10:08:48 +0900

    update

--- Test_dynmaic_graphs/test_dynamic.py
+++ Test_dynmaic_graphs/test_dynamic.py
@@ -85,7 +85,7 @@ def _count_max_deg(graphs, adjs):
 def _get_degree_from_adj(adj, num_nodes, graph):
     # print(adj.todense())
 
-    degs_out = dict(nx.degree(graph)).value()
+    degs_out = dict(nx.degree(graph)).values()
     degs_in = degs_out
 
     # adj_tensor = torch.tensor(adj.todense())
