ac5ff681a841458bc7bc092daf93b4d7a1facdb6
Fahao Chen <chenfh612@gmail.com>
2022-06-26 21:22:57 +0900

    1

--- Test_dynmaic_graphs/Epinion/preprocess.py
+++ Test_dynmaic_graphs/Epinion/preprocess.py
@@ -60,9 +60,9 @@ from datetime import datetime, timedelta
 collect data from 'START_DATE' and ends to 'END_DATE'.
 generate a graph per 'SLICE_DAYS'.
 '''
-SLICE_DAYS = 6
-START_DATE = min(ts) + timedelta(100)
-END_DATE = min(ts) + timedelta(400)
+SLICE_DAYS = 8
+START_DATE = min(ts) + timedelta(10)
+END_DATE = min(ts) + timedelta(500)
 
 print ("Start date", START_DATE)
 print ("End date", END_DATE)
