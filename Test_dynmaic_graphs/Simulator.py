fe8b6c642872851277abfd68ab8b1d6e091bf815
Fahao Chen <chenfh612@gmail.com>
2022-07-09 17:02:51 +0900

    1

--- Test_dynmaic_graphs/Simulator.py
+++ Test_dynmaic_graphs/Simulator.py
@@ -606,7 +606,7 @@ class divide_and_conquer():
                 Cross_node.append(Cross_nodes(self.timesteps, self.workloads_GCN[m], P_snapshot[idx]))
             Cross_edge = [ce*self.args['beta'] for ce in Cross_edge]
             result = np.sum([Load,Cross_edge],axis=0).tolist()
-            result = np.sum([Load,Cross_edge],axis=0).tolist()
+            result = np.sum([Load,Cross_node],axis=0).tolist()
             
             # np.multiply(Load, Cross_edge).tolist()
             select_m = result.index(max(result))
