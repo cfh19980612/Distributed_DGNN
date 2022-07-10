9a430c5935d2f595fb46f23b474b0b30ff0da7a9
Fahao Chen <chenfh612@gmail.com>
2022-07-10 15:59:01 +0900

    1

--- Test_dynmaic_graphs/Simulator.py
+++ Test_dynmaic_graphs/Simulator.py
@@ -197,7 +197,7 @@ def Cross_edges(timesteps, adjs, nodes_list, Degrees, current_workload, workload
         # print(idx.size())
         # print(idx.reshape([idx.size(0)*2, -1]).size())
         has_nodes = torch.nonzero(current_workload[time][idx] == True, as_tuple=False).view(-1)
-        print('all degrees: ',sum(Degrees[time]))
+        # print('all degrees: ',sum(Degrees[time]))
         num += has_nodes.size(0)/sum(Degrees[time])
 
     # node-graph cross edges at multiple timesteps
@@ -213,7 +213,7 @@ def Cross_edges(timesteps, adjs, nodes_list, Degrees, current_workload, workload
         nodes_idx = edge_target[idx]
         # print(nodes_idx)
         has_nodes = torch.nonzero(current_workload[time][nodes_idx] == True, as_tuple=False).view(-1)
-        print('all degrees: ',sum(Degrees[time]))
+        # print('all degrees: ',sum(Degrees[time]))
         num += has_nodes.size(0)/sum(Degrees[time])
 
 
