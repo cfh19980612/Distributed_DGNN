155b53d3975e3b82c847a3ce0563e21e49cb32ba
Fahao Chen <chenfh612@gmail.com>
2022-07-10 14:26:15 +0900

    1

--- Test_dynmaic_graphs/Simulator.py
+++ Test_dynmaic_graphs/Simulator.py
@@ -181,6 +181,7 @@ def Cross_edges(timesteps, adjs, nodes_list, current_workload, workload, flag):
         edge_target = adj._indices()[1]
         idx_list = [torch.nonzero(edge_source == node, as_tuple=False).view(-1) for node in nodes]
         nodes_idx_list = [edge_target[idx] for idx in idx_list]
+        print(nodes_idx_list)
         nodes_idx = torch.cat((nodes_idx_list), dim=0)
         has_nodes = torch.nonzero(current_workload[time][nodes_idx] == True, as_tuple=False).view(-1)
         num += has_nodes.size(0)
