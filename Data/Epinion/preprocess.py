0ad56b24716d551822961f71842a54852ff7be4d
Fahao Chen <chenfh612@gmail.com>
2022-07-22 08:45:17 +0900

    1

--- Data/Epinion/preprocess.py
+++ Data/Epinion/preprocess.py
@@ -60,7 +60,7 @@ from datetime import datetime, timedelta
 collect data from 'START_DATE' and ends to 'END_DATE'.
 generate a graph per 'SLICE_DAYS'.
 '''
-SLICE_DAYS = 12
+SLICE_DAYS = 13
 START_DATE = min(ts) + timedelta(100)
 END_DATE = min(ts) + timedelta(800)
 
